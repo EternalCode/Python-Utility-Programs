cf34c9f2d1021fe77969481c08a4e0466427a2b0
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2015-11-04 11:48:49 -0500

    quick update
    Nothing much to say, changed algorithm slightly.

--- asciiHexMedium.py
+++ asciiHexMedium.py
@@ -1,10 +1,10 @@
-with open('C:/Users/Dilshan/Documents/Hack/Sprites/Table.txt') as f:
+with open('C:/Table.txt') as f:
     lines = f.read().splitlines()
 for i in range (0, len(lines)):
     lines[i] = lines[i].split('=')
 convert = raw_input("enter string: ")
 result = ""
-check_version = raw_input('H2A or A2H?')
+check_version = raw_input('hex to ascii or ascii to hex?')
 if (check_version == "1"):
     for char in convert:
         for element in lines:
@@ -12,9 +12,13 @@ if (check_version == "1"):
                 result += (element[0])
     print(result)
 else:
-    convert = convert.split(" ")
-    for index in convert:
+    #take 2 letters at a time from convert
+    counter = 0
+    #strip whitespace from convert
+    convert = convert.replace(" ", "")
+    while counter < len(convert):
         for element in lines:
-            if element[0] == index:
+            if element[0] == (convert[counter:counter+2]):
                 result += (element[1])
+        counter += 2
     print(result)
